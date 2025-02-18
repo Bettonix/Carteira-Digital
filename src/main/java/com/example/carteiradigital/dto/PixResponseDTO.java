debd5fd2ab5779108d3cbe6e11dffcdc6f51be79
Carlos Bettoni <carlorsbettoni@gmail.com>
2025-02-18 16:30:27 -0400

    fix: Alterando tipo de dado da varíavel valor de Double para BigDecimal.

--- src/main/java/com/example/carteiradigital/dto/PixResponseDTO.java
+++ src/main/java/com/example/carteiradigital/dto/PixResponseDTO.java
@@ -3,6 +3,7 @@ package com.example.carteiradigital.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 public class PixResponseDTO {
     private String idTransacao;
     private String status;
-    private Double valor;
+    private BigDecimal valor;
     private String origem;
     private String destino;
     private LocalDateTime data;
