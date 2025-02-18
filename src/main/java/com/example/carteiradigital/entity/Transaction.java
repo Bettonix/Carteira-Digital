dfc8cac739f38261b575979d557ee1de831ac078
Carlos Bettoni <carlorsbettoni@gmail.com>
2025-02-18 12:51:10 -0400

    fix: Adicionando Import java.math.BigDecimal

--- src/main/java/com/example/carteiradigital/entity/Transaction.java
+++ src/main/java/com/example/carteiradigital/entity/Transaction.java
@@ -2,6 +2,7 @@ package com.example.carteiradigital.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
